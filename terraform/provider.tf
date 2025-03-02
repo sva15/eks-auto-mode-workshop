13c75d67badd2848e80f881a0746fe51343b15b8
Sandip Das <sandip.sandip.das5@gmail.com>
2025-03-02 22:56:56 +0530

    removed required provider version not needed

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,7 @@ provider "aws" {
 
 terraform {
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14.0"
-    }
+    
   }
   required_version = ">= 1.3.0"
 }
